87a98a3b316b1803b8202408d7ee4df3bd5957fe
kkurbatskyi <57308179+kkurbatskyi@users.noreply.github.com>
2020-08-21 01:14:27 -0700

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,16 +1,6 @@
-#include <iostream>
-
 //n - number of a fibonacci sequence
 long fibonacci(int n)
 {
 	if(n <= 2) return 1;
 	return fibonacci(n - 1) + fibonacci(n - 2);
-}
-
-int main()
-{
-	int num; //argument to pass in a fibonacci function
-	std::cin >> num;	
-	std::cout << fibonacci(num) << std::endl;
-	system("pause");
 }
